0b0bd477de678734f332eca9d470bd52ba4514dc
Stefanie Engstrom <38473399+s3ngstrom@users.noreply.github.com>
2022-12-18 18:12:12 -0600

    Changes from module 24

---
+++ index.js
@@ -0,0 +1,78 @@
+console.log("Before!");
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  console.log("Loaded!");
+});
+
+console.log("After!");
+
+const firstBtn = document.querySelector("button");
+firstBtn.addEventListener("click", (event) => {
+  console.log(event.target);
+});
+
+// Select all the buttons for all the parks
+const allBtns = document.querySelectorAll(".rate-button");
+
+// Iterate through the list of buttons and add an event handler to each
+allBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    console.log(event.target);
+  });
+});
+
+allBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    console.log(event.target.parentNode);
+  });
+});
+
+allBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    const park = event.target.parentNode;
+    park.style.backgroundColor = "#c8e6c9";
+  });
+});
+
+// Select the `nameSorter` link
+const nameSorter = document.querySelector("#name-sorter");
+
+// Add an event listener
+nameSorter.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log("You clicked the name sorter");
+});
+
+nameSorter.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  // 1.  Get the main element
+  const main = document.querySelector("main");
+
+  // 2. Get the list of parks
+  const parksList = main.querySelectorAll(".park-display");
+
+  // 3. Empty the main element
+  main.innerHTML = "";
+
+  // 4. Create an array
+  const parksArray = Array.from(parksList);
+
+  // 5. Sort the array
+  parksArray.sort((parkA, parkB) => {
+    const parkAName = parkA.querySelector("h2").innerText;
+    const parkBName = parkB.querySelector("h2").innerText;
+    if (parkAName < parkBName) {
+      return -1;
+    } else if (parkAName > parkBName) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  // 6. Insert each park into the DOM
+  parksArray.forEach((park) => {
+    main.appendChild(park);
+  });
+});
